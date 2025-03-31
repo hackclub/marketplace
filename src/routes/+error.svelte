240abb1598d24c72eeb0e8a1727bcd010a430800
mart <93423789+ThatFrogDev@users.noreply.github.com>
2025-03-31 12:22:50 +0200

    basic 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<div class="min-h-screen flex flex-col items-center justify-center">
+	<h1 class="text-4xl text-center text-red-500 font-semibold" style="font-family: Phantom Sans;">
+		yikes!
+	</h1>
+	<p style="font-family: Phantom Sans;">strange, orpheus surely isn't told about this page...</p>
+	<div class="mt-4">
+		<button
+			style="font-family: Phantom Sans;"
+			class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl"
+		>
+			go back home
+		</button>
+	</div>
+</div>
