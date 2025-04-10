b3ecf9bf4711022839e49d7f1cc64ff98326c6fa
Echo <github@3kh0.net>
2025-04-09 22:13:30 -0400

    lint fix 3

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,20 +3,28 @@
 	import { onMount } from 'svelte';
 	let { children } = $props();
 	function isMobile() {
-        return /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent) || navigator.maxTouchPoints > 1 || window.visualViewport.width <= 576;
-    }
-    onMount(() => {
-        console.log("Git version: ", window.__GIT_COMMIT_HASH__)
-        if (isMobile() && window.location.pathname !== "/no-mobile" && !localStorage.getItem("im_not_on_mobile_i_promise")) {
-            window.location.href = "/no-mobile"; // no mobile smh
-        }
-    });
+		return (
+			/Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent) ||
+			navigator.maxTouchPoints > 1 ||
+			window.visualViewport.width <= 576
+		);
+	}
+	onMount(() => {
+		console.log('Git version: ', window.__GIT_COMMIT_HASH__);
+		if (
+			isMobile() &&
+			window.location.pathname !== '/no-mobile' &&
+			!localStorage.getItem('im_not_on_mobile_i_promise')
+		) {
+			window.location.href = '/no-mobile'; // no mobile smh
+		}
+	});
 </script>
+
+{@render children()}
 
 <style>
 	:global(body) {
-		background-color: #F4DECF;
+		background-color: #f4decf;
 	}
 </style>
-
-{@render children()}
