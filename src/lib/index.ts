f06547c8a7810c7a3a00fdc160d157295e808359
Echo <github@3kh0.net>
2025-04-09 22:13:04 -0400

    lint fix 2

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,3 @@
 // place files you want to import through the `$lib` alias in this folder.
 
-
-
-
-
-export const ngrokUrl = "https://6579-2606-a300-9016-b5a-1c1a-a8ad-b649-7ca5.ngrok-free.app";
+export const ngrokUrl = 'https://6579-2606-a300-9016-b5a-1c1a-a8ad-b649-7ca5.ngrok-free.app';
