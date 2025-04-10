154cf734791c001b1fadb7ce2a20ffa1d5bad550
Echo <github@3kh0.net>
2025-04-09 22:13:04 -0400

    lint fix 2

--- src/lib/slack-oauth.ts
+++ src/lib/slack-oauth.ts
@@ -1,4 +1,5 @@
-import { PUBLIC_SLACK_CLIENT_ID, PUBLIC_REDIRECT_URL } from "$env/static/public";
+import { PUBLIC_SLACK_CLIENT_ID, PUBLIC_REDIRECT_URL } from '$env/static/public';
+
 export function getCallbackURL() {
-    return `https://hackclub.slack.com/oauth/v2/authorize?scope=&user_scope=identity.basic&redirect_uri=${encodeURIComponent(PUBLIC_REDIRECT_URL + "/api/oauth/slack/callback")}&client_id=${PUBLIC_SLACK_CLIENT_ID}`
+	return `https://hackclub.slack.com/oauth/v2/authorize?scope=&user_scope=identity.basic&redirect_uri=${encodeURIComponent(PUBLIC_REDIRECT_URL + '/api/oauth/slack/callback')}&client_id=${PUBLIC_SLACK_CLIENT_ID}`;
 }
