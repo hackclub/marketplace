a8a615ae1ad0bef07b82bb9cf3b292800bff672b
Neon <neon@saahild.com>
2025-04-18 20:56:34 +0000

    feat: bye bye!

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,11 @@
 import { sequence } from "@sveltejs/kit/hooks";
 import { handleErrorWithSentry, sentryHandle } from "@sentry/sveltekit";
 import * as Sentry from '@sentry/sveltekit';
+import { ADMIN_LOGIN } from "$env/static/private";
 import prisma from "$lib/prisma";
-
+import { dev } from "$app/environment";
+// yes im using this, no i dont care that this is bad code
+let isinMaintinince = true;
 Sentry.init({
   dsn: 'https://77f6aed8c8164ba7e7c3aa47b7e839f6@o4508373315026944.ingest.us.sentry.io/4509129843671040',
 
@@ -16,6 +19,18 @@ Sentry.init({
 // If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
 export const handle = sequence(sentryHandle(),async function ({ event, resolve }) {
   console.log(event.url.pathname)
+  if (isinMaintinince) {
+    const auth = event.request.headers.get("Authorization");
+    if (auth !== `Basic ${btoa(ADMIN_LOGIN)}` && !dev) {
+        return new Response("Not authorized", {
+            status: 401,
+            headers: {
+                "WWW-Authenticate":
+                    'Basic realm="User Visible Realm", charset="UTF-8"',
+            },
+        });
+    }
+  }
   const cookie = event.cookies.get('session')
   if (cookie && !event.cookies.get('onboarded') && event.url.pathname !== "/onboard" && event.url.pathname !== "/api/settings/update") {
     const userData = await prisma.user.findFirst({
