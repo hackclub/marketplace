9baf87fd4d2d4bebb88888255944db9436765034
Echo <github@3kh0.net>
2025-05-02 15:36:01 -0400

    format file to spec

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,28 +1,30 @@
-import { handleErrorWithSentry, replayIntegration } from "@sentry/sveltekit";
+import { handleErrorWithSentry, replayIntegration } from '@sentry/sveltekit';
 import * as Sentry from '@sentry/sveltekit';
-import type { Handle } from "@sveltejs/kit";
+import type { Handle } from '@sveltejs/kit';
 
 Sentry.init({
-  dsn: 'https://77f6aed8c8164ba7e7c3aa47b7e839f6@o4508373315026944.ingest.us.sentry.io/4509129843671040',
+	dsn: 'https://77f6aed8c8164ba7e7c3aa47b7e839f6@o4508373315026944.ingest.us.sentry.io/4509129843671040',
 
-  tracesSampleRate: 1.0,
+	tracesSampleRate: 1.0,
 
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+	// This sets the sample rate to be 10%. You may want this to be 100% while
+	// in development and sample at a lower rate in production
+	replaysSessionSampleRate: 0.1,
 
-  // If the entire session is not sampled, use the below sample rate to sample
-  // sessions when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
-  environment: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  // maskAllText: false,
-  // blockAllMedia: false,
-  // If you don't want to use Session Replay, just remove the line below:
-  integrations: [replayIntegration({
-    maskAllText: false,
-  blockAllMedia: false,
-  networkDetailAllowUrls: [window.location.origin],
-  })],
+	// If the entire session is not sampled, use the below sample rate to sample
+	// sessions when an error occurs.
+	replaysOnErrorSampleRate: 1.0,
+	environment: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+	// maskAllText: false,
+	// blockAllMedia: false,
+	// If you don't want to use Session Replay, just remove the line below:
+	integrations: [
+		replayIntegration({
+			maskAllText: false,
+			blockAllMedia: false,
+			networkDetailAllowUrls: [window.location.origin]
+		})
+	]
 });
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,71 +1,76 @@
-import { sequence } from "@sveltejs/kit/hooks";
-import { handleErrorWithSentry, sentryHandle } from "@sentry/sveltekit";
+import { sequence } from '@sveltejs/kit/hooks';
+import { handleErrorWithSentry, sentryHandle } from '@sentry/sveltekit';
 import * as Sentry from '@sentry/sveltekit';
-import { ADMIN_LOGIN } from "$env/static/private";
-import prisma from "$lib/prisma";
-import { dev } from "$app/environment";
+import { ADMIN_LOGIN } from '$env/static/private';
+import prisma from '$lib/prisma';
+import { dev } from '$app/environment';
 // yes im using this, no i dont care that this is bad code
 let isinMaintinince = false;
 Sentry.init({
-  dsn: 'https://77f6aed8c8164ba7e7c3aa47b7e839f6@o4508373315026944.ingest.us.sentry.io/4509129843671040',
+	dsn: 'https://77f6aed8c8164ba7e7c3aa47b7e839f6@o4508373315026944.ingest.us.sentry.io/4509129843671040',
 
-  tracesSampleRate: 1.0,
-  environment: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+	tracesSampleRate: 1.0,
+	environment: process.env.NODE_ENV === 'production' ? 'production' : 'development'
 
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: import.meta.env.DEV,
+	// uncomment the line below to enable Spotlight (https://spotlightjs.com)
+	// spotlight: import.meta.env.DEV,
 });
 
 // If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
-export const handle = sequence(sentryHandle(),async function ({ event, resolve }) {
-  console.log(event.url.pathname)
-  if (isinMaintinince) {
-    const auth = event.request.headers.get("Authorization");
-    if (auth !== `Basic ${btoa(ADMIN_LOGIN)}` && !dev && !event.url.pathname.startsWith("/api/")) {
-        return new Response("Not authorized", {
-            status: 401,
-            headers: {
-                "WWW-Authenticate":
-                    'Basic realm="User Visible Realm", charset="UTF-8"',
-            },
-        });
-    }
-  }
-  const cookie = event.cookies.get('session')
-  if (cookie && !event.cookies.get('onboarded') && event.url.pathname !== "/onboard" && event.url.pathname !== "/api/settings/update") {
-    const userData = await prisma.user.findFirst({
-      where: {
-        token: cookie
-      }
-    })
+export const handle = sequence(sentryHandle(), async function ({ event, resolve }) {
+	console.log(event.url.pathname);
+	if (isinMaintinince) {
+		const auth = event.request.headers.get('Authorization');
+		if (auth !== `Basic ${btoa(ADMIN_LOGIN)}` && !dev && !event.url.pathname.startsWith('/api/')) {
+			return new Response('Not authorized', {
+				status: 401,
+				headers: {
+					'WWW-Authenticate': 'Basic realm="User Visible Realm", charset="UTF-8"'
+				}
+			});
+		}
+	}
+	const cookie = event.cookies.get('session');
+	if (
+		cookie &&
+		!event.cookies.get('onboarded') &&
+		event.url.pathname !== '/onboard' &&
+		event.url.pathname !== '/api/settings/update'
+	) {
+		const userData = await prisma.user.findFirst({
+			where: {
+				token: cookie
+			}
+		});
 
-    if (userData) {
-      if (!(userData.address && userData.hcb_email && userData.reigions_for_shipping)) {
-        // console.log(`DAWG LEMME IN `)
-        return new Response('You are NOT skipping onboarding', {
-          status: 302,
-          headers: {
-            location: '/onboard?a=1'
-          }
-        });
-      }
-      if (!event.cookies.get('user-info')) { 
-         event.cookies.set(`user-info`, JSON.stringify({
-          slack_name: userData.slack_name,
-          slack_id: userData.slack_id,
-          hcb_email: userData.hcb_email
-        }), { path: "/", expires: new Date(Date.now() + 60 * 1000),  httpOnly: false })
-        Sentry.setUser({token: cookie, email: userData.hcb_email});
-
-      }
-     
-    }
-    
-  } 
+		if (userData) {
+			if (!(userData.address && userData.hcb_email && userData.reigions_for_shipping)) {
+				// console.log(`DAWG LEMME IN `)
+				return new Response('You are NOT skipping onboarding', {
+					status: 302,
+					headers: {
+						location: '/onboard?a=1'
+					}
+				});
+			}
+			if (!event.cookies.get('user-info')) {
+				event.cookies.set(
+					`user-info`,
+					JSON.stringify({
+						slack_name: userData.slack_name,
+						slack_id: userData.slack_id,
+						hcb_email: userData.hcb_email
+					}),
+					{ path: '/', expires: new Date(Date.now() + 60 * 1000), httpOnly: false }
+				);
+				Sentry.setUser({ token: cookie, email: userData.hcb_email });
+			}
+		}
+	}
 
 	const response = await resolve(event);
 	return response;
-})
+});
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
 export const handleError = handleErrorWithSentry();
